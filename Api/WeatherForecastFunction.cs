6222f54019c1b6bb27a64d951ec74bc94e2ede49
Ritesh Raj <Raj.Ritesh9@outlook.com>
2022-03-05 01:42:40 +0530

    Reverting change to fix build issue

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -18,35 +18,35 @@ namespace BlazorApp.Api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://community-open-weather-map.p.rapidapi.com/find?q=london&cnt=0&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric"),
-                Headers =
-                {
-                     { "x-rapidapi-host", "community-open-weather-map.p.rapidapi.com" },
-                     { "x-rapidapi-key", "d4dc087f9bmshebfe337ca60511fp17a3d9jsn05ee4b43eebb" },
-                },
-            };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var result = await response.Content.ReadAsStringAsync();
-                return new OkObjectResult(result);
-                //Console.WriteLine(body);
-            }
-            //var randomNumber = new Random();
-            //var temp = 0;
-
-            //var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //var client = new HttpClient();
+            //var request = new HttpRequestMessage
             //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = temp = randomNumber.Next(-20, 55),
-            //    Summary = GetSummary(temp)
-            //}).ToArray();
+            //    Method = HttpMethod.Get,
+            //    RequestUri = new Uri("https://community-open-weather-map.p.rapidapi.com/find?q=london&cnt=0&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric"),
+            //    Headers =
+            //    {
+            //         { "x-rapidapi-host", "community-open-weather-map.p.rapidapi.com" },
+            //         { "x-rapidapi-key", "d4dc087f9bmshebfe337ca60511fp17a3d9jsn05ee4b43eebb" },
+            //    },
+            //};
+            //using (var response = await client.SendAsync(request))
+            //{
+            //    response.EnsureSuccessStatusCode();
+            //    var result = await response.Content.ReadAsStringAsync();
+            //    return new OkObjectResult(result);
+            //    //Console.WriteLine(body);
+            //}
+            var randomNumber = new Random();
+            var temp = 0;
 
-            return null;
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
+
+            return new OkObjectResult(result);
         }
     }
 }
